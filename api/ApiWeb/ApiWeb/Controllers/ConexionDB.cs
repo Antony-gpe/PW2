61ad807e5f221f531dd49ee2b3b7610f2cf73976
luis.ignacio <luis.ignacio@grupotimexico.com>
2023-10-15 22:41:38 -0600

    se agrega tablas relacionales de base de datos y se agrega construccion de api con autentificacion, endpoin consulta a login (http://localhost:5048/ConsultasBD/login?nombreUsuario=buny&contrasena=1234) donde nombreUsuario y contrasena son parametros para realizar la consulta, nota importante la base de datos esta creada con el usuario root y sin password de cambiar de usuario y agregar password estos datos se pueden cambiar en el archivo de configuracion de la api llamado appsettings.json

---
+++ api/ApiWeb/ApiWeb/Controllers/ConexionDB.cs
@@ -0,0 +1,15 @@
+﻿using ApiWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Usuario> Usuario { get; set; }
+    // Otras propiedades DbSet para otras tablas
+
+    // ...
+}
